2c3ed40024fbaabcffee632f9f2a970d32def491
Ben Patterson <bmizepatterson@gmail.com>
2018-11-06 12:22:34 -0500

    Fix typo in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
                 <a href="{{ route('login') }}">login</a>
 
                 @if (Route::has('register'))
-                    <a href="{{ route('register') }}">legister</a>
+                    <a href="{{ route('register') }}">register</a>
                 @endif
             @endauth
         </div>
