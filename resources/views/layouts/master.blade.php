74a66d31d352f8b6a3af31eb06a01864d6691133
Ben Patterson <bmizepatterson@gmail.com>
2018-11-09 09:49:31 -0500

    Force https in production

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,13 +10,13 @@
     <title>@yield('title')</title>
 
     <!-- Scripts -->
-    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
     <!-- Styles -->
-    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
